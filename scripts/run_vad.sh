38523e65f947120b8a65c408bc56f305eeee753d
Nacho <nacho.sancho.p@gmail.com>
2022-10-24 12:37:13 +0200

    run_vad.sh update

--- scripts/run_vad.sh
+++ scripts/run_vad.sh
@@ -4,7 +4,7 @@
 # Use the nautilus explorer or chmod +x run_vad.sh
 
 # Write here the name and path of your program and database
-HOMELOCAL=$HOME/Desktop/Uni/3B          # Path to your home directory !!!
+HOMELOCAL=$HOME/...         # Path to your home directory !!!
 DIR_P2=$HOMELOCAL/PAV/P2-1
 DB=$DIR_P2/db.v4
 CMD=$DIR_P2/bin/vad
